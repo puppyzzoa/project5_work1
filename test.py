942536e280f0b3ceb0e3ead803ba9386cfe72184
puppyzzoa <sy_2283@naver.com>
2024-09-05 21:06:02 +0900

    커밋했다

---
+++ test.py
@@ -0,0 +1,18 @@
+# 두 숫자를 더하는 함수
+def add_numbers(a, b):
+    return a + b
+
+# 예시 실행
+result = add_numbers(3, 5)
+print(f"두 숫자의 합: {result}")
+
+
+# 두 숫자를 빼는 함수
+def subtract_numbers(a, b):
+    return a - b
+
+# 예시 실행
+result = subtract_numbers(10, 4)
+print(f"두 숫자의 차: {result}")
+
+# end
